0e7ca1a21ed82b9c12ebc0344dd484aab76311b9
Ricardo Rojas <reddrum7@gmail.com>
2014-12-16 10:51:59 -0500

    Add form style wiht bootstrap

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -2,7 +2,7 @@ class ArticlesController < ApplicationController
   http_basic_authenticate_with name: "dhh", password: "secret", except: [:index, :show]
 
   def index
-    @articles = Article.all
+    @articles = Article.all.order(created_at: :desc).limit(10)
   end
 
   def new
@@ -21,7 +21,7 @@ class ArticlesController < ApplicationController
     @article = Article.new(article_params)
 
     if @article.save
-      redirect_to @article
+      render 'create'
     else
       render 'new'
     end
